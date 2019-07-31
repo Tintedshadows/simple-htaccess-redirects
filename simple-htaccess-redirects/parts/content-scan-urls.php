8c7f0a6ab68a4ddaa9d0d12d2f92ccef961bcf82
Tintedshadows <33048640+Tintedshadows@users.noreply.github.com>
2019-07-31 16:32:05 -0500

    Add files via upload

---
+++ simple-htaccess-redirects/parts/content-scan-urls.php
@@ -0,0 +1,19 @@
+<div class="wrap container" id="scan-urls">
+    
+<div>
+    <h1>Scan Urls</h1>
+    
+
+<button id="GetFile">Download CSV Of Links</button>
+	
+<button id="ScanWorkingUrls">Get Site Urls</button>
+	
+	
+
+	<div id="UrlContainer">
+		
+	</div>
+	
+</div>
+
+</div>
